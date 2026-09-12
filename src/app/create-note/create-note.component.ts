5ef0208f3555634f02dcd036478bc83ae3cdc14d
=Mitch3012 <mitch3012.jcm@gmail.com>
2026-09-12 10:46:09 +0100

    Create Working

--- src/app/create-note/create-note.component.ts
+++ src/app/create-note/create-note.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TaskBoard } from '../task-board-service';
+import { Task } from '../task';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators  } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [ReactiveFormsModule],
   selector: 'app-create-note.component',
   styleUrl: './create-note.component.css',
   templateUrl: './create-note.component.html',
 })
-export class CreateNoteComponent {}
+export class CreateNoteComponent {
+    taskService = inject(TaskBoard);
+    taskForm = new FormGroup({
+      title: new FormControl('',[Validators.required]),
+      description: new FormControl('', [(Validators.required)]),
+      complete: new FormControl,
+    })
+    onSubmit(){
+      console.log(this.taskForm.value);
+      if (this.taskForm.valid){
+        const newTask: Task =  {
+          id: crypto.randomUUID(),
+          title: this.taskForm.value.title!,
+          description: this.taskForm.value.description!,
+          completed: this.taskForm.value.complete!,
+        
+        };
+        this.taskService.addTask(newTask);
+      }
+    }
+}
